78bd795adccef27fa93cb25af52cc22b9e2596e7
pss28 <47633545+pss28@users.noreply.github.com>
2020-11-12 17:17:49 -0300

    Add files via upload

---
+++ Aparecida_Nutricao/js/calcula-imc.js
@@ -0,0 +1,58 @@
+var titulo = (document.querySelector('.titulo'));
+titulo.textContent = 'Aparecida Nutricionista';
+
+var pacientes = document.querySelectorAll('.paciente');
+
+for (var i = 0; i < pacientes.length; i++) {
+
+  var paciente = pacientes[i];
+  var tdPeso = paciente.querySelector('.info-peso');
+  var tdAltura = paciente.querySelector('.info-altura');
+  var tdImc = paciente.querySelector('.info-imc');
+
+  var peso = tdPeso.textContent;
+  var altura = tdAltura.textContent;
+
+  var pesoEhValido = validaPeso(peso);
+  var alturaEhValida = validaAltura(altura);
+
+  if (!pesoEhValido) {
+    pesoEhValido = false;
+    tdImc.textContent = 'Peso inválido';
+    paciente.classList.add('paciente-invalido');
+  }
+
+  if (!alturaEhValida) {
+    alturaEhValida = false;
+    tdImc.textContent = 'Altura inválida';
+    paciente.classList.add('paciente-invalido');
+  }
+
+  if (alturaEhValida && pesoEhValido) {
+    var imc = calculaImc(peso, altura);
+    tdImc.textContent = imc;
+  }
+}
+
+function validaPeso(peso) {
+    if (peso >= 0 && peso < 1000) {
+      return true;
+    }else {
+      return false;
+    }
+}
+
+function validaAltura(altura) {
+  if (altura >= 0 && altura <= 3.0) {
+    return true;
+  }else {
+    return false;
+  }
+
+}
+
+function calculaImc(peso, altura) {
+  var imc = 0;
+  imc = peso / (altura * altura);
+  return imc.toFixed(2);
+}
